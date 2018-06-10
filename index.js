87ff93077726ada18baf678e9f9b0d649a7ee6e4
jose barboza <jrbarboza08@gmail.com>
2018-06-10 20:32:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,9 +26,12 @@ console.log(string.toLowerCase())
 
 function sayHiToGrandam(string){
   
-  if (string.toLowerCase === string){
+  if (string.toLowerCase() === string){
     return "I can't hear you"
-  } else if (string.toUpperCase)
+  } else if (string.toUpperCase() === string){
+    return "YES INDEED"
+    
+  }
   
   
 }
