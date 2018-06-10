cf6c520efb31e6bc70dabbf3faed2b4db5a87ae6
jose barboza <jrbarboza08@gmail.com>
2018-06-10 20:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ function sayHiToGrandam(string){
   } else if (string.toUpperCase() === string){
     return "YES INDEED"
     
+  }else if (string === "I love you, Grandman.") {
+    return "I love you, too"
+    
+    
     
   }
   
